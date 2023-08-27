f2cab27b539f41082148fb07e26949a6f34de9a2
Henrique Milani <henriquewmilani@gmail.com>
2023-08-27 20:44:06 -0300

    - 'login.dart' code refactor for better performance and readability - Added a standard theme with a few of our colors to 'main.dart', we should now be able to get all the colors we need for the next pages from there - Fixed an issue where opening the keyboard on the login screen would cause a few widgets to overflow

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,30 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MoveOut',
+      //Day Theme Prototype:
       theme: ThemeData(
         useMaterial3: true,
         primaryColor: Color(0xFF6fa8dc),
         primaryColorDark: Color(0xFF7ca4b7),
+        colorScheme: ColorScheme(
+          primary: Color(0xFF6fa8dc),
+          onPrimary: Color(0xFF7ca4b7),
+          secondary: Color(0xFFf1c232),
+          onSecondary: Color(0xFFe4b82f),
+          background: Color(0xFFffffff),
+          onBackground: Color(0xFFeeeeee),
+          surface: Color(0xFFfbfbfb),
+          onSurface: Color(0xFF000000),
+          brightness: Brightness.light,
+          error: Color(0xFFBA0027),
+          onError: Color(0xFF000000),
+        ),
         fontFamily: 'Open Sans',
+        
       ),
       home: const MyHomePage(),
     );
@@ -38,9 +52,21 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        top: false,
-        child: AuthScreen(),
+      body: SingleChildScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+          minWidth: MediaQuery.of(context).size.width,
+          minHeight: MediaQuery.of(context).size.height,
+        ),
+        child: IntrinsicHeight(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: const <Widget>[
+              AuthScreen(),
+            ],
+          )
+        ))
       )
     );
   }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,6 +1,3 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
-import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -9,171 +6,160 @@ void main() {
   runApp(const AuthScreen());
 }
 
-class AuthScreen extends StatefulWidget {
+class AuthScreen extends StatelessWidget {
   const AuthScreen({super.key});
 
   @override
-  State<AuthScreen> createState() => _AuthScreenState();
-}
-
-class _AuthScreenState extends State<AuthScreen> {
-  @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(
+      const SystemUiOverlayStyle(
         statusBarColor: Colors.transparent,
+        systemNavigationBarContrastEnforced: true,
       ),
     );
-    return Scaffold(
-      body: Center(
-        child: Container(
-          width: MediaQuery.sizeOf(context).width,
-          height: MediaQuery.sizeOf(context).height,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/backgrounds/mbl_bg_3.png'),
-              fit: BoxFit.cover,
+    return Container(
+      width: MediaQuery.sizeOf(context).width,
+      height: MediaQuery.sizeOf(context).height,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/backgrounds/mbl_bg_3.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: SafeArea(
+        minimum: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
+              child: Image(
+                alignment: Alignment.center,
+                image: AssetImage('assets/images/logos/logo1.png'),
+                fit: BoxFit.fitWidth,
+              ),
             ),
-          ),
-          child: SafeArea(
-            minimum: const EdgeInsets.all(15.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-                  child: Image(
-                    alignment: Alignment.center,
-                    image: AssetImage('assets/images/logos/logo1.png'),
-                    fit: BoxFit.fitWidth,
-                  ),
+            const SizedBox(height: 50),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxWidth: MediaQuery.sizeOf(context).width,
+                  minHeight: MediaQuery.sizeOf(context).height * 0.35,
                 ),
-                SizedBox(height: 50),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ConstrainedBox(
-                    constraints: BoxConstraints(
-                      maxWidth: MediaQuery.sizeOf(context).width,
-                      minHeight: MediaQuery.sizeOf(context).height * 0.35,
-                    ),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                        color: Color.fromARGB(255, 111, 168, 220),
-                      ),
-                      alignment: Alignment.center,
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 25.0, right: 25.0, top: 25.0, bottom: 10.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(15)),
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  alignment: Alignment.center,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 25.0, right: 25.0, top: 25.0, bottom: 10.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const LoginTextWidget(lbl: 'E-mail ou CPF:', obscure: false),
+                        const SizedBox(height: 50),
+                        const Column(
                           children: [
-                            TextField(
-                              decoration: InputDecoration(
-                                enabledBorder: UnderlineInputBorder(
-                                    borderSide: BorderSide(
-                                  width: 1,
-                                  color: Colors.white,
-                                )),
-                                focusedBorder: UnderlineInputBorder(
-                                  borderSide: const BorderSide(
-                                      color: Colors.white, width: 1.0),
-                                ),
-                                labelText: "E-mail ou CPF:",
-                                labelStyle:
-                                    TextStyle(color: Colors.white, fontSize: 15),
-                              ),
-                            ),
-                            SizedBox(height: 50),
-                            Column(
-                              children: [
-                                TextField(
-                                  obscureText: true,
-                                  decoration: InputDecoration(
-                                    enabledBorder: UnderlineInputBorder(
-                                        borderSide: BorderSide(
-                                      width: 1,
-                                      color: Colors.white,
-                                    )),
-                                    focusedBorder: UnderlineInputBorder(
-                                      borderSide: const BorderSide(
-                                          color: Colors.white, width: 1.0),
-                                    ),
-                                    labelText: "Senha:",
-                                    labelStyle: TextStyle(
-                                        color: Colors.white, fontSize: 15),
-                                  ),
-                                ),
-                                TextButton(
-                                  onPressed: () {},
-                                  child: 
-                                    Align(
-                                      alignment: Alignment.centerRight,
-                                      child: Text(
-                                        "Esqueci a Senha",
-                                        textDirection: TextDirection.ltr,
-                                        style: TextStyle(
-                                          fontSize: 11,
-                                          color: Colors.white,
-                                          decoration:
-                                            TextDecoration.underline,
-                                            decorationColor: Colors.white,
-                                            decorationThickness: 2,
-                                        ),
-                                      ),
-                                    )
-                                )
-                              ],
-                            ),
-                            SizedBox(height: 60),
-                            Center(
-                              child: FilledButton(
-                                onPressed: () {},
-                                style: ButtonStyle(
-                                  backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFf1c232))
-                                ),
-                                child: const Text('Entrar', textDirection: TextDirection.ltr),
-                              )
-                            ),
-                            SizedBox(height: 20),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  "Ainda não possui uma conta?",
-                                  textDirection: TextDirection.ltr,
-                                  style: TextStyle(
-                                    fontSize: 11,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                TextButton(onPressed: () {}, child: 
-                                  Text("Cadastre-se",
-                                    textDirection: TextDirection.ltr,
-                                    style: TextStyle(
-                                      fontSize: 11,
-                                      color: Colors.amber,
-                                      decoration: 
-                                        TextDecoration.underline,
-                                        decorationColor: Colors.amber,
-                                        decorationThickness: 2,
-                                    ),
-                                  )
-                                )
-                              ],
-                            )
+                            LoginTextWidget(lbl: 'Senha:', obscure: true),
+                            LoginTextButton(txt: 'Esqueci a Senha', color: Colors.white),
                           ],
                         ),
-                      ),
+                        const SizedBox(height: 40),
+                        Center(
+                          child: FilledButton(
+                            onPressed: () {},
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(Theme.of(context).colorScheme.secondary),
+                              fixedSize: MaterialStateProperty.all(const Size(200, 60)),
+                            ),
+                            child: const Text(
+                              'Entrar',
+                              textDirection: TextDirection.ltr,
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                        )),
+                        const SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              "Ainda não possui uma conta?",
+                              textDirection: TextDirection.ltr,
+                              style: TextStyle(
+                                fontSize: 11,
+                                color: Colors.white,
+                              ),
+                            ),
+                            LoginTextButton(txt: 'Cadastre-se', color: Theme.of(context).colorScheme.secondary),
+                          ],
+                        )
+                      ],
                     ),
                   ),
-                )
-              ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginTextButton extends StatelessWidget {
+  final String txt;
+  final Color color;
+  const LoginTextButton({super.key, required this.txt, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: () {},
+        child: Align(
+          alignment: Alignment.centerRight,
+          child: Text(
+            txt,
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 11,
+              color: color,
+              decoration: TextDecoration.underline,
+              decorationColor: color,
+              decorationThickness: 2,
             ),
           ),
+        ));
+  }
+}
+
+class LoginTextWidget extends StatelessWidget {
+  final String lbl;
+  final bool obscure;
+  const LoginTextWidget({super.key, required this.lbl, required this.obscure});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: obscure,
+      style: const TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+        enabledBorder: const UnderlineInputBorder(
+            borderSide: BorderSide(
+          width: 1,
+          color: Colors.white,
+        )),
+        focusedBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.white, width: 1.0),
         ),
-      )
+        labelText: lbl,
+        labelStyle: const TextStyle(color: Colors.white, fontSize: 15),
+      ),
     );
   }
 }
