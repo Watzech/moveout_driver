71f4bc52390db31855348d86caf95fdd2c7a8e8c
João Arruda <joao.oliveira.arruda06@gmail.com>
2023-09-05 02:42:11 -0300

    feat: connecting signup with mongodb

--- lib/constants/main.dart
+++ lib/constants/main.dart
@@ -1,3 +1,3 @@
-const String URL_CONNECTION = 'mongodb+srv://<username>:<password>@<cluster>.mongodb.net/?retryWrites=true&w=majority';
+const String URL_CONNECTION = 'mongodb+srv://project_moveout:0liw4zsa7@cluster.xbh5ebw.mongodb.net/?retryWrites=true&w=majority';
 
-const String CLIENT_COLLECTION = 'Client Collection';
+const String CLIENT_COLLECTION = 'CAD_CLIENTE';
